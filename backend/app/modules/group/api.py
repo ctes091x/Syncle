a797721cbd527ea20bf8f69ce8a1582e4237492b
nasu <nasu@mma.club.uec.ac.jp>
2025-12-30 01:25:26 +0900

    core/の修正に伴うgroup/apiの修正

--- backend/app/modules/group/api.py
+++ backend/app/modules/group/api.py
@@ -4,7 +4,7 @@ from typing import List
 
 # 依存関係 (プロジェクト構成に合わせて適宜調整してください)
 from app.core.database import get_db
-from app.core.security import get_current_user
+from app.core.dependencies import get_current_user
 from app.modules.user.models import User
 
 from . import crud, schemas, models
